15e940392d9d46f2d8e34700243eb57b93ae5c03
Ayyaz Bin Mumtaz <ayyazbinmumtaz3@gmail.com>
2024-12-12 14:35:10 +0500

    initial commit

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -7,8 +7,11 @@ import { carSchema } from "../validations/carValidation.js";
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const upload = multer({ dest: 'uploads/' });
+
+// const upload = multer({ dest: 'uploads/' });
 
 router.post('/add-car', authMiddleware, validatorMiddleWare(carSchema), upload.array('pictures', 10), carController.addCar);
 
